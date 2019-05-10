9d45068c007493f173d0face7a974f84e8db1dee
Gabrielle Bartomeo <31347690+gabartomeo@users.noreply.github.com>
2019-05-10 14:27:18 -0400

    Working on Final

--- Final Project/gabartomeo_final.py
+++ Final Project/gabartomeo_final.py
@@ -11,8 +11,12 @@ import plotly.graph_objs as go
 # Table 2: Affordable rent for unit size by percentages of the AMI
 ny_ami = pd.read_html("https://www1.nyc.gov/site/hpd/renters/area-median-income.page")
 
-# This gives us the total number of households in NYC based on household size # Works
+# This gives us the total number of households in NYC based on household size in 2000 # Works
 nyc_hhs = pd.read_excel("data/DEC_00_SF4_HCT005 (1).xls")
 nyc_hhs = nyc_hhs.iloc[4:,[0,2]]
-nyc_hhs.rename(columns={list(nyc_hhs)[0]: "Household Size", list(nyc_hhs)[1]:"Number of Households"}, inplace=True)
+nyc_hhs.columns = ["Household Size", "Number of Households"]
 
+# This gives us the number of individuals living in NYC during the 2000 Census # Works
+nyc_census_summary = pd.read_html("https://www1.nyc.gov/site/planning/data-maps/nyc-population/census-summary-2000.page")[0]
+nyc_census_summary = nyc_census_summary.iloc[1:, [0,3]]
+nyc_census_summary.columns = ["Boroughs", "Population in 2000"]
